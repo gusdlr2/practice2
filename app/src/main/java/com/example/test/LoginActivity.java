c06b0f307e39c4e2729060b3a129923c00cd8201
didgu <didgusdlr@naver.com>
2023-04-18 18:54:11 +0900

    test44

--- app/src/main/java/com/example/test/LoginActivity.java
+++ app/src/main/java/com/example/test/LoginActivity.java
@@ -15,7 +15,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+// test1
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     @Override
